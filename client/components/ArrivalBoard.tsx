0963f0b03a32148e35627ab6e3f66658e658bec5
Sean Outhwaite <sean.outhwaite@gmail.com>
2025-11-17 15:20:36 +1300

    Get seemingly correct data on arrival board

--- client/components/ArrivalBoard.tsx
+++ client/components/ArrivalBoard.tsx
@@ -1,9 +1,9 @@
-import {  useLocations } from '../hooks.tsx'
+import {  useTrips } from '../hooks.tsx'
 // import { tripFilter } from '../locationHandler.ts'
 
 
 const ArrivalBoard = ()=> {
-  const {data, isError, isPending, error} = useLocations()
+  const {data, isError, isPending, error} = useTrips()
 
   if (isPending) return <img src='/giphy.gif' alt='moving bus'/>
 
@@ -13,21 +13,29 @@ const ArrivalBoard = ()=> {
   }
 
   // const updates = (tripFilter(JSON.parse(data.data).response.entity))
-  // const timeConvert = function(t: number){
-  //   const dateObj = new Date(t * 1000)
-  //   return dateObj
-  // }
-console.log(JSON.parse(data.data))
+  const timeConvert = function(t: string, d: number){
+    const dateObj = new Date(`2025-11-17T${t}`)
+
+    const time = dateObj.getTime() + (d* 1000)
+    console.log(dateObj.getTime())
+
+    const arrival = new Date(time).toLocaleTimeString('en-US')
+   return arrival
+  }
+
+console.log(data)
   return (
 
    <>
-   {/* {updates.map((t)=> (
-    <div key={t.id}>
-      <p>{t.trip_update.trip.route_id}</p>
-      <p>{String(timeConvert(t.trip_update.stop_time_update.arrival.time))}</p>
+   {data.map((t)=> (
+    <div key={t.id}className='mb-4 text-sm font-extrabold leading-none tracking-tight text-gray-900 md:text-sm lg:text-sm dark:text-white'>
+      <p>{t.attributes.route_id}</p>
+      <p>{t.attributes.arrival_time}</p>
+      <p>{timeConvert(t.attributes.arrival_time, t.delay)}</p>
+      <p>{t.delay}</p>
     </div>
 
-   ))} */}
+   ))}
     </>
   )
 }
--- server/routes/trips.ts
+++ server/routes/trips.ts
@@ -20,6 +20,8 @@ router.get('/', async (req, res) => {
   const finalFormattedDate = `${year}-${month}-${day}`
   const currentHour = date.getHours()
 
+  const dateTemplate = `${year}-${month}-${day}T`
+
   const routeIDs = [
   '25B-202',
   '25L-202',
@@ -31,25 +33,52 @@ router.get('/', async (req, res) => {
   '30-202',
 ]
   try {
+    // Fetches static schedule data
     const response = await request
       .get(
-        `https://api.at.govt.nz/gtfs/v3/stops/7151-93995941/stoptrips?filter[date]=${finalFormattedDate}&filter[start_hour]=${currentHour}&filter%5Bhour_range%5D=2`,
+        `https://api.at.govt.nz/gtfs/v3/stops/7151-93995941/stoptrips?filter[date]=${finalFormattedDate}&filter[start_hour]=${currentHour -1}&filter%5Bhour_range%5D=2`,
       )
       .set('Ocp-Apim-Subscription-Key', `${apiKey}`)
     const stop = JSON.parse(response.text)
       const filtered: Trips[]= []
 
-  // const date = new Date()
-  // const time = date.toTimeString().slice(0,8)
-
+  
+  // Filters for the routes I care about
     stop.data.forEach((x: Trips )=>{
     if (routeIDs.includes(x.attributes.route_id) )
       filtered.push(x)
     })
 
+    const ids = filtered.map((t)=> t.attributes.trip_id)
+    // Fetches trip updates from the live API
+    const updates = await request
+          .get(
+            `https://api.at.govt.nz/realtime/legacy/tripupdates?tripid=${ids}`,
+          )
+          .set('Ocp-Apim-Subscription-Key', `${apiKey}`)
 
-    
-    res.json(filtered
+    const delays = JSON.parse(updates.text).response.entity
+
+
+    // Combines the delays from the trip updates feed with the static schedule data
+    const withDelays = filtered.map((t)=>{
+      const delay = delays.find((d)=> d.id === t.attributes.trip_id)
+      if (delay){
+        return {...t, delay: delay.trip_update.delay}
+      } else {
+        return {...t, delay:0}
+      }
+    })
+
+    const currentTrips = withDelays.filter((t)=> {
+      const dateString = new Date(dateTemplate + t.attributes.arrival_time)
+      const unixTime = dateString.getTime()
+
+      return unixTime + (t.delay * 1000) > date.getTime()
+
+    })
+    console.log(currentTrips)
+    res.json(currentTrips
     )
   } catch (err) {
     console.log(err)
