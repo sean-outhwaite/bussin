82f595f5aa68b204fe8a928aa35f4ef9923b082a
Sean Outhwaite <sean.outhwaite@gmail.com>
2026-08-18 11:12:25 +1200

    Disable route filtering

--- server/transform.ts
+++ server/transform.ts
@@ -1,16 +1,16 @@
 import {  stopTrip } from '../models/location'
 
-const routeIDs = [
-  '25B-202',
-  '25L-202',
-  '27W-202',
-  '27H-202',
-  '22N-202',
-  '22R-202',
-  '24R-202',
-  '30-202',
-  '24B-202'
-]
+// const routeIDs = [
+//   '25B-202',
+//   '25L-202',
+//   '27W-202',
+//   '27H-202',
+//   '22N-202',
+//   '22R-202',
+//   '24R-202',
+//   '30-202',
+//   '24B-202'
+// ]
 
 export function getTrips(stopTrips: stopTrip) {
   const trips: string[] = []
@@ -18,7 +18,7 @@ export function getTrips(stopTrips: stopTrip) {
   const time = date.toTimeString().slice(0,8)
 
   stopTrips.data.forEach((x) => {
-    if (routeIDs.includes(x.attributes.route_id) && x.attributes.arrival_time > time )
+    if (/*routeIDs.includes(x.attributes.route_id) &&*/ x.attributes.arrival_time > time )
       trips.push(x.attributes.trip_id)
   })
   return trips
