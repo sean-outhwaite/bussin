822bc8cd8dc7feaff6843c44303821f93c832ed4
Sean Outhwaite <sean.outhwaite@gmail.com>
2026-08-21 12:31:30 +1200

    Fix issue where selection wasn't working if the stop had a duplicate name

--- client/pages/MapDisplay.tsx
+++ client/pages/MapDisplay.tsx
@@ -39,39 +39,52 @@ const MapDisplay = () => {
 
   return (
     <>
-  
     <div className='flex justify-center'>
  <Select 
  onChange={(e)=> {
-        const stop = stops.find((s)=> s.stop_name === e?.value)
+        const stop = stops.find((s)=> s.stop_code === e?.value)
         if (stop) setSelectedStop(stop)
       }}
- value={{value:selectedStop.stop_name, label:`${selectedStop.stop_name} - ${selectedStop.stop_code}`}}
+ value={{value:selectedStop.stop_code, label:`${selectedStop.stop_name} - ${selectedStop.stop_code}`}}
  styles = {{
   container: (provided) => ({
     ...provided,
     width: '50%',
-    paddingBottom: '1rem',
   }),
   control: (provided) => ({
     ...provided,
     backgroundColor: '#ebf5fb',
-    color: '#2d7caf',
     fontWeight: 'bold',
+  }),
+  singleValue: (provided) => ({
+    ...provided,
+    color: '#2d7caf',
+  }),
+  dropdownIndicator: (provided)=> ({
+    ...provided,
+    color:  '#cb0076',
+    '&:hover': {
+      color: '#2d7caf',
+    }
+  }),
+ indicatorSeparator: (provided)=> ({
+    ...provided,
+    backgroundColor: '#2d7caf',
   }),
  menu: (provided) => ({
     ...provided,
     color: '#2d7caf',
     fontWeight: 'bold',
     backgroundColor: '#ebf5fb',
+    marginTop: 0,
   }), 
- }} options = {stops.map((s) => ({ value: s.stop_name, label: `${s.stop_name} - ${s.stop_code}` }))} />
+ }} options = {stops.map((s) => ({ value: s.stop_code, label: `${s.stop_name} - ${s.stop_code}` }))} />
     </div>
     <div className='flex justify-center'>
       {latLongs && (
           <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_KEY}>
             <Map
-              style={{ width: '80vw', height: '50vh' }}
+              style={{ width: '80vw', height: '50vh', paddingTop:'1rem' }}
               center={{ lat: selectedStop.stop_lat, lng: selectedStop.stop_lon }}
               defaultZoom={15}
               gestureHandling="greedy"
